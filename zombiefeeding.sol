e31d8b05581f25a06013e0cad1757c1527291b58
Jun Woo Shin <jwoos@users.noreply.github.com>
2018-01-13 17:03:43 -0500

    Update zombiefeeding.sol

--- zombiefeeding.sol
+++ zombiefeeding.sol
@@ -23,8 +23,14 @@ contract KittyInterface {
 // inheritance
 contract ZombieFeeding is ZombieFactory {
 	// instantiate the interface to the cryptokitties contract
-	address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
-	KittyInterface kittyContract = KittyInterface(ckAddress);
+	// don't use a hard coded address
+	address ckAddress;
+	KittyInterface kittyContract;
+	
+	// address setter
+	function setKittyContractAddress(address _address) external {
+		kittyContract = KittyInterface(_address);
+	}
 
 	function feedAndMultiply(uint _zombieId, uint _targetDna, string species) public {
 		require(msg.sender == zombieToOwner[_zombieId]);
